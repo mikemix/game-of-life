01c6f562fc6a7de9aae2d279af81f0c0a2a0bfc7
Michał Żukowski <mzukowski@accesto.pl>
2017-08-29 09:21:35 +0200

    Probability fix

--- src/Game/RandomMatrix.php
+++ src/Game/RandomMatrix.php
@@ -6,9 +6,9 @@ final class RandomMatrix extends AbstractMatrix2D
 {
     private $probability;
 
-    public function __construct(int $width, int $height, int $probability = null)
+    public function __construct(int $width, int $height, int $probability = 3)
     {
-        $this->probability = $probability ?: 3;
+        $this->probability = $probability;
         if ($this->probability < 1 || $this->probability > 9) {
             throw new \InvalidArgumentException('Probability between 1-9 expected');
         }
@@ -16,7 +16,7 @@ final class RandomMatrix extends AbstractMatrix2D
         $cells = [];
         for ($x = 0; $x < $height; $x++) {
             for ($y = 0; $y < $width; $y++) {
-                $cells[$x][$y] = mt_rand(0, $this->probability) < 2 ? Cell::alive() : Cell::dead();
+                $cells[$x][$y] = mt_rand(1, 10) < $this->probability ? Cell::alive() : Cell::dead();
             }
         }
 
